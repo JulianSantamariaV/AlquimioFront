6b1dbb9f7d8b1cc39a912763fac2bf0554e1fb0a
Juan David Sandoval <judsandovalca@unal.edu.co>
2025-02-14 22:10:52 -0500

    login

--- src/components/cards/CardLogin.tsx
+++ src/components/cards/CardLogin.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,42 +6,80 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import React from "react";
+
 
 const CardLogin: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const baseURL = "http://localhost:3000/auth/login";
+ // const [post, setPost] = React.useState(null);
+  function createPost( password: any, email: any) { // change any
+    axios
+      .post(baseURL, {
+       password,
+       email
+      })
+      .then((response) => {
+        console.log(response.data)
+      });
+  }
+
   return (
     <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle className="text-center">LOGIN</CardTitle>
-        <CardDescription></CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
+      <form
+        onSubmit={handleSubmit((data) =>
+        { console.log(data)
+          createPost(data.password,data.email)
+        }
+       
+        )}
+      >
+        <CardHeader>
+          <CardTitle className="text-center">LOGIN</CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
           <div className="grid w-full items-center gap-4">
-
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="Email"></Label>
-              <Input type="email" id="Email" placeholder="Corre Electronico" />
+              <Input
+                {...register("email")}
+                type="email"
+                id="Email"
+                placeholder="Corre Electronico"
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="Password"></Label>
-              <Input type="password" id="password" placeholder="Contraseña" />
+              <Input
+                {...register("password")}
+                type="password"
+                id="password"
+                placeholder="Contraseña"
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Link to="/" >
-          <Button variant="destructive">Cancelar</Button>
-        </Link>
-        {/* INGRESAR */}
-        <Button>Ingresar</Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Link to="/">
+            <Button variant="destructive">Cancelar</Button>
+          </Link>
+          {/* INGRESAR */}
+          <Button type="submit">Ingresar</Button>
+        </CardFooter>
+      </form>
     </Card>
-  )
-}
+  );
+};
 
 export default CardLogin;
