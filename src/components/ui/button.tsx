3bc8d5fae437196fbdcdc47603f763a4d5b8970e
julian santamaria <juliangsv2@hotmail.com>
2025-03-09 00:57:42 -0500

    .

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        important: "bg-amber-600 text-primary-foreground shadow-xs hover:bg-amber-700 active:bg-amber-600/90 cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
--- src/pages/products/ProductHome.tsx
+++ src/pages/products/ProductHome.tsx
@@ -1,71 +1,49 @@
-
 import ProductCard from "@/components/cards/CardProduct";
-import ProductWideCard from "@/components/cards/CardProductWIde";
-import { CarouselMine } from "@/components/carousel/CarouselMine";
-import { ServicesIcons } from "@/components/carousel/CarouselServicesIcons";
-import SectionTitle from "@/components/title/SectionTitle";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
 import { useProducts } from "@/hooks/useProduct";
-import { useVisibleProducts } from "@/hooks/useVisibleProduct";
+import { PlusIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const ProductHome = () => {
   const { products, loading, error } = useProducts();
-  const visibleProducts = useVisibleProducts();
+
+  console.log(products);
+
+  if (loading) return <p className="text-center">Cargando productos...</p>;
+  if (error) return <p className="text-center text-gray-700">{error}</p>;
 
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <div className="w-4/5 mx-auto py-6">
-        <CarouselMine carouselImg={["/placeholder.jpg", "/placeholder.jpg", "/placeholder.jpg"]} />
-      </div>
 
-      <section className="container mx-auto max-w-7xl p-6">
-        {/* Productos Destacados */}
-        <SectionTitle title="Productos Destacados" />
-        {loading ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-6">
-            {[...Array(visibleProducts)].map((_, i) => (
-              <Skeleton key={i} className="h-48 w-full rounded-xl" />
-            ))}
-          </div>
-        ) : error ? (
-          <p className="text-center text-gray-700">{error}</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 my-6 justify-items-center">
+    <div className="grid grid-cols-5 grid-rows-6 gap-4">
+      <div className="mx-auto my-5 flex flex-col items-center justify-center col-span-4 row-span-2">
 
-            {products.slice(0, visibleProducts).map((product) => (
-              
+        <div className="flex justify-between items-center w-full">
+          <h1 className="text-2xl font-bold text-slate-800">Ofertas de hoy</h1>
+          <Button asChild size={"lg"} variant={"important"}>
+            <Link to="/products/ProductCreate">
+            Vender
+              <PlusIcon className="w-5 h-5" />              
+            </Link> 
+          </Button>
+        </div>
+
+
+        <hr></hr>
+
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-4 my-6">
+          {products.slice(0, 6).map((product) => (
+            <div key={product.name} className="w-full">
               <ProductCard key={product.name} {...product} />
-              
-            ))
-            }
-          </div>
-        )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr></hr>
+      <div className="col-span-5 row-span-2 row-start-5">
 
-        <hr className="my-6 border-gray-300" />
-
-        {/* Nuestros Servicios */}
-        <SectionTitle title="Nuestros Servicios" />
-        <ServicesIcons />
-
-        <hr className="my-6 border-gray-300" />
-
-        {/* Ofertas Especiales */}
-        <SectionTitle title="Ofertas Especiales" />
-        {loading ? (
-          <div className="flex flex-col gap-4 my-6">
-            {[...Array(2)].map((_, i) => (
-              <Skeleton key={i} className="h-28 w-full rounded-xl" />
-            ))}
-          </div>
-        ) : (
-          <div className="flex flex-col gap-4 my-6">
-            {products.slice(3, 5).map((product) => (
-              <ProductWideCard key={product.name} {...product} />
-            ))}
-          </div>
-        )}
-      </section>
+      </div>
     </div>
+
   );
 };
 
